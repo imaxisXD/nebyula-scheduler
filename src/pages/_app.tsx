44499a7d2c9d538dd5c455382b90540ef46cbc89
imaxisXD <sunny735084@gmail.com>
2023-05-05 20:55:48 +0530

    feat: added auth-clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import { type AppType } from "next/app";
-
+import { ClerkProvider, SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <SignedIn>
+        <Component {...pageProps} />
+      </SignedIn>
+      <SignedOut>
+        <SignIn
+          appearance={{
+            elements: {
+              formButtonPrimary: 'bg-slate-500 hover:bg-slate-400 text-sm normal-case'
+            }
+          }}
+        />
+      </SignedOut>
+    </ClerkProvider>);
 };
 
 export default api.withTRPC(MyApp);
