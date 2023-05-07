3eb63d2468fa7cfe6169034105fbd4adfa4cd4e5
imaxisXD <sunny735084@gmail.com>
2023-05-08 03:56:22 +0530

    Fixed: nav bar and daywise component

--- src/pages/availabilty.tsx
+++ src/pages/availabilty.tsx
@@ -1,18 +1,40 @@
 import { type NextPage } from "next";
 import Navbar from "~/component/ui/navbar";
+import { BellRing, Check } from "lucide-react"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "../component/ui/card"
+import DayWiseCard from "~/component/dayWiseCard";
+
 
 const availabilty: NextPage = () => {
     // const { data } = api.getCurrentUser.getAll.useQuery();
     // console.log(data);
-
-
+    // const dateHourArray = [
+    //     { hrs:}
+    // ]
+    const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     return (
         <>
             <Navbar />
+            <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#e9e9e9] to-[#eea9fdd2]">
+                <Card className="m-20">
+                    <CardHeader>
+                        <CardTitle>Availabilty Hours</CardTitle>
+                        <CardDescription>Mark your hours when you are avaialble for meetings</CardDescription>
+                    </CardHeader>
+                    <CardContent >
+                        {weekday.map((day, index) => (
+                            <DayWiseCard name={day} key={index} />
+                        ))}
 
-            <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+                    </CardContent>
+                </Card>
 
-                Hi
             </main>
         </>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#e9e9e9] to-[#da88ec]">
 
 
       </main>
